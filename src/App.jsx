c9590233d3cfe50334bcf4626bfd714a7dcd9238
= <=>
2025-10-14 00:18:15 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css'
+import "./index.css";
+
+function App() {
+  return(
+    <div className="main-window">
+      <div className="phone-window">
+        <div className="welcome-text">
+          <h1>Epic Game</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
+
